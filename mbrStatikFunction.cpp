b26035dd6b5a6dde1cc698d6ed2900e480ee5c9d
akmaalsyh <taufansyah50@gmail.com>
2024-05-14 15:07:46 +0700

    memasukkan variabel void mahasiswa print all

--- mbrStatikFunction.cpp
+++ mbrStatikFunction.cpp
@@ -30,3 +30,10 @@ void mahasiswa::setId()
 {
     id = ++nim;
 }
+
+void mahasiswa::printAll()
+{
+    cout << "ID     = " << id << endl;
+    cout << "Nama   = " << nama << endl;
+    cout << endl;
+}
