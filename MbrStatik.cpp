a222b352291dd9577b043430fbcd6d43f822eac5
akmaalsyh <taufansyah50@gmail.com>
2024-05-14 14:34:12 +0700

    memasukkan variabel class mahasiswa

--- MbrStatik.cpp
+++ MbrStatik.cpp
@@ -1,2 +1,19 @@
 #include <iostream>
 using namespace std;
+
+class mahasiswa
+{
+public:
+    static int nim;
+    int id;
+    string nama;
+
+    void setId();
+
+    void printAll();
+    mahasiswa(string pnama)
+    {
+        nama = pnama;
+        setId();
+    }
+}
