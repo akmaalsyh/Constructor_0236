2227916327a4fb9f41b991b281f44227148350ff
akmaalsyh <taufansyah50@gmail.com>
2024-05-14 15:45:30 +0700

    memasukkan variabel constructor

--- destructor.cpp
+++ destructor.cpp
@@ -13,3 +13,11 @@ public:
     void cetakData();
     void isiData();
 };
+
+// Definisi member Function
+angka::angka(int i)  // Constructor
+{
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}
