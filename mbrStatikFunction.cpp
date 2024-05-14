405b7218f0c397014c323a2026a8cd4d6c78e459
akmaalsyh <taufansyah50@gmail.com>
2024-05-14 15:01:52 +0700

    memasukkan variabel class mahasiswa dan objek nya

--- mbrStatikFunction.cpp
+++ mbrStatikFunction.cpp
@@ -1,3 +1,25 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class mahasiswa
+{
+private:
+    static int nim;
+
+public:
+    int id;
+    string nama;
+
+    void setId();
+    void printAll();
+
+    static void setNim(int pnim) { nim = pnim; }
+    static int getNim () { return nim; }
+
+    mahasiswa(string pnama)
+    {
+        nama = pnama;
+        setId();
+    }
+};
